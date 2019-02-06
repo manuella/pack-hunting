b87451e6d2c655d1d8c210886a6094db7dfeb58d
EvanFManuella@gmail.com <evan.manuella@fdmgroup.com>
2019-02-05 20:41:20 -0500

    add basic transactions component that fetches from remote mock

---
+++ src/app/classes/transaction.ts
@@ -0,0 +1,7 @@
+export class Transaction
+{
+  transactionId:number;
+  debitorId : number;
+  creditorId : number;
+  amount: number;
+}
---
+++ src/app/services/transaction.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Transaction } from '../classes/transaction';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TransactionService {
+
+  constructor(private httpService : HttpClient) { }
+
+  transactions: Transaction[];
+
+  public getTransactions()
+  {
+    return this.httpService.get<Transaction[]>("http://www.mocky.io/v2/5c3ffb7d3500006d2eec3ab1");
+  }
+}

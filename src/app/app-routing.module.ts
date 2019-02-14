5a396eb2b86f69f2c4c62ff3ed5b16e868c0343d
Evan <EvanFManuella@gmail.com>
2019-02-14 17:32:13 -0500

    Add MVP accpet offer template with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { TransactionsComponent } from './transactions/transactions.component';
 import { AccountsComponent } from './accounts/accounts.component';
 import { RegisterAccountComponent } from './register-account/register-account.component';
 import { SellToBrokerComponent } from './sell-to-broker/sell-to-broker.component';
+import { AcceptOfferComponent } from './accept-offer/accept-offer.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/transactions', pathMatch: 'full' },
   { path: 'transactions', component: TransactionsComponent },
   { path: 'accounts', component: AccountsComponent },
   { path: 'register-account', component: RegisterAccountComponent },
-  { path: 'sell-question', component: SellToBrokerComponent}
+  { path: 'sell-question', component: SellToBrokerComponent},
+  { path: 'accept-offer', component: AcceptOfferComponent}
 ];
 
 @NgModule({

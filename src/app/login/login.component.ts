ea546b31e9cdf1e5967c55816d82637600563375
Evan <EvanFManuella@gmail.com>
2019-02-17 20:49:10 -0500

    update login to use shorthand formbuilder

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { AccountService } from '../services/account.service';
 
 @Component({
   selector: 'app-login',
@@ -8,16 +11,20 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  loginForm = new FormGroup({
-    accountId: new FormControl('')
+
+  loginForm = this.fb.group({
+    accountId:['', Validators.required]
   });
 
-  onSubmit() {
-  // TODO: Use EventEmitter with form value
-  console.warn(this.loginForm);
-}
 
-  constructor() { }
+
+
+//  onSubmit() {
+  // TODO: Use EventEmitter with form value
+//  console.warn(this.loginForm);
+//}
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
   }
